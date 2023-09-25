6df8f1a6e5131d43ef57367134d1f74a524baad9
Barry Adams <barry.david.adams@gmail.com>
2023-09-25 21:29:16 +0100

    master fixed compile and running

--- src/main/java/com/smokpromotion/SmokProm/config/portal/MajoranaAccessDeniedHandler.java
+++ src/main/java/com/smokpromotion/SmokProm/config/portal/MajoranaAccessDeniedHandler.java
@@ -13,7 +13,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDate;
