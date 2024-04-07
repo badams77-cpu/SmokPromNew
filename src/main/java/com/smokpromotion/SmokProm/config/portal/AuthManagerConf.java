4374f2e989460658412d008f39eb2629086de96c
Barry Adams <barry.david.adams@gmail.com>
2024-04-07 22:31:20 +0100

    auth_token

--- src/main/java/com/smokpromotion/SmokProm/config/portal/AuthManagerConf.java
+++ src/main/java/com/smokpromotion/SmokProm/config/portal/AuthManagerConf.java
@@ -44,132 +44,14 @@ public class AuthManagerConf {
     @Autowired
     private MajoranaAccessDecisionManager decisionManager;
 
+    private PortalWebSecurityConfig  pwsc;
 
-
-    @Bean
-    @Order(-500)
-    SecurityWebFilterChain springWebFilterChain() {
-
-        ServerHttpSecurity http = pwsc.getServerHttpSecurity();
-
-        if (http==null){
-            LOGGER.error("springWebFilterChain not set serverHttpSecurity is null");
-        }
-
-        Customizer flc = new Customizer<ServerHttpSecurity.FormLoginSpec>() {
-            @Override
-            public void customize(ServerHttpSecurity.FormLoginSpec fld) {
-            }
-        };
-
-        ServerAuthenticationSuccessHandler ash = new ServerAuthenticationSuccessHandler(){
-            @Override
-            public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
-
-                LOGGER.warn("Auth Success "+authentication.isAuthenticated()+" "+authentication.getName()
-                        +" from "+ webFilterExchange.getExchange().getRequest().getRemoteAddress() );
-
-                ServerHttpResponse swe = webFilterExchange.getExchange().getResponse();
-
-                // Set HTTP status code to 302 (Found) for redirection
-                swe.setStatusCode(HttpStatus.FOUND);
-                swe.getHeaders().setLocation(URI.create(LOGGED_IN_HOME_PAGE));
-
-                return swe.setComplete();
-
-       //         webFilterExchange.getExchange().getResponse().writeAndFlushWith()
-                //return null;.
-            }
-
-   //         public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-   //             response.sendRedirect();
-   //         }
-            // LOGGED_IN_HOME_PAGE
-        };
-
-        ReactiveAuthorizationManager<AuthorizationContext> ram =  new ReactiveAuthorizationManager<AuthorizationContext>() {
-            @Override
-            public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext object) {
-                return checkAccess( authentication, object);
-            }
-        };
-
-    /*{
-        @Override
-        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-            response.sendRedirect();
-        }
-    };*/
-
-        //http // registerMatcher( a-> a
-        http
-
-                .authorizeExchange( b->b.anyExchange().access(ram));
-        //   .and()
-        //   .anyExchange().denyAll();;
-
-        http.formLogin()
-                .authenticationSuccessHandler( ash);
-
-        //   http
-        //.logoutSuccessHandler(lsh)
-
-        return http.build();
-    }
-
-        private PortalWebSecurityConfig pwsc;
-
-        @Autowired
+    @Autowired
     public AuthManagerConf(PortalWebSecurityConfig pwsc){
-            this.pwsc = pwsc;
-        }
-
-//    @Autowired
-//    private HttpSecurity httpSecurity;
-
-    @Lazy
-    @Bean()
-    @Order(-500)
-    public ReactiveAuthenticationManager reactiveAuthManager() throws Exception {
-        HttpSecurity http = pwsc.getHttpSecurity();
-
-        ReactiveAuthenticationManager authenticationManager =
-                http.getSharedObject(ReactiveAuthenticationManager.class);
-   //     this.reactiveAuthenticationManager = authenticationManager;
-        return authenticationManager;
+        this.pwsc = pwsc;
     }
 
-    @Lazy
-    @Bean
-    @Order(-500)
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(pwsc.getPortalCustomAuthenticationProvider());
-        return authenticationManagerBuilder.build();
-    }
 
-    public Mono<AuthorizationDecision> checkAccess(Mono authentication, Object object) {
-        Authentication auth = (Authentication) authentication.block();
-
-        AuthorizationContext context = (AuthorizationContext) object;
-
-        try {
-
-            PortalSecurityPrinciple prin = (PortalSecurityPrinciple) auth.getPrincipal();
-            return Mono.just(new AuthorizationDecision(decisionManager.checkAccessForPath(
-                    context.getExchange().getRequest(),
-                    auth,
-                    context.getExchange().getRequest().getPath().contextPath().value())));
-            //               hasRole("ADMIN").check(authentication, context)
-            //           .switchIfEmpty(hasRole("DBA")
-            //                                        .check(authentication, context))
-
-        } catch (Exception e) {
-            LOGGER.warn("IO exception ", e);
-        }
-        return Mono.just( new AuthorizationDecision(false));
-    }
 /*
     @Bean
     @Order(-500)
--- src/main/java/com/smokpromotion/SmokProm/config/portal/PortalWebSecurityConfig.java
+++ src/main/java/com/smokpromotion/SmokProm/config/portal/PortalWebSecurityConfig.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authorization.AuthorizationDecision;
@@ -36,6 +38,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
@@ -53,6 +56,7 @@ import reactor.core.publisher.Mono;
 import javax.naming.Context;
 
 import java.io.IOException;
+import java.net.URI;
 
 import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.anyExchange;
 
@@ -74,7 +78,10 @@ public class PortalWebSecurityConfig implements WebSecurityConfigurer<SecurityBu
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MajoranaAccessDecisionManager.class);
 
+    private static final String LOGGED_IN_HOME_PAGE = "/portal/private/home.html";
 
+    @Autowired
+    private MajoranaAccessDecisionManager decisionManager;
 
     @Autowired
     private MajoranaCustomAPISecurityFilter majoranaCustomAPISecurityFilter;
@@ -362,6 +369,128 @@ public class PortalWebSecurityConfig implements WebSecurityConfigurer<SecurityBu
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+
+    @Bean
+    @Order(-500)
+    SecurityWebFilterChain springWebFilterChain() {
+
+        ServerHttpSecurity http = pwsc.getServerHttpSecurity();
+
+        if (http==null){
+            LOGGER.error("springWebFilterChain not set serverHttpSecurity is null");
+        }
+
+        Customizer flc = new Customizer<ServerHttpSecurity.FormLoginSpec>() {
+            @Override
+            public void customize(ServerHttpSecurity.FormLoginSpec fld) {
+            }
+        };
+
+        ServerAuthenticationSuccessHandler ash = new ServerAuthenticationSuccessHandler(){
+            @Override
+            public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+
+                LOGGER.warn("Auth Success "+authentication.isAuthenticated()+" "+authentication.getName()
+                        +" from "+ webFilterExchange.getExchange().getRequest().getRemoteAddress() );
+
+                ServerHttpResponse swe = webFilterExchange.getExchange().getResponse();
+
+                // Set HTTP status code to 302 (Found) for redirection
+                swe.setStatusCode(HttpStatus.FOUND);
+                swe.getHeaders().setLocation(URI.create(LOGGED_IN_HOME_PAGE));
+
+                return swe.setComplete();
+
+                //         webFilterExchange.getExchange().getResponse().writeAndFlushWith()
+                //return null;.
+            }
+
+            //         public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            //             response.sendRedirect();
+            //         }
+            // LOGGED_IN_HOME_PAGE
+        };
+
+        ReactiveAuthorizationManager<AuthorizationContext> ram =  new ReactiveAuthorizationManager<AuthorizationContext>() {
+            @Override
+            public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext object) {
+                return checkAccess( authentication, object);
+            }
+        };
+
+    /*{
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            response.sendRedirect();
+        }
+    };*/
+
+        //http // registerMatcher( a-> a
+        http
+
+                .authorizeExchange( b->b.anyExchange().access(ram));
+        //   .and()
+        //   .anyExchange().denyAll();;
+
+        http.formLogin()
+                .authenticationSuccessHandler( ash);
+
+        //   http
+        //.logoutSuccessHandler(lsh)
+
+        return http.build();
+    }
+
+    private PortalWebSecurityConfig pwsc;
+
+
+
+//    @Autowired
+//    private HttpSecurity httpSecurity;
+
+    @Lazy
+    @Bean()
+    @Order(-500)
+    public ReactiveAuthenticationManager reactiveAuthManager() throws Exception {
+        HttpSecurity http = pwsc.getHttpSecurity();
+
+        ReactiveAuthenticationManager authenticationManager =
+                http.getSharedObject(ReactiveAuthenticationManager.class);
+        //     this.reactiveAuthenticationManager = authenticationManager;
+        return authenticationManager;
+    }
+
+    @Lazy
+    @Bean
+    @Order(-500)
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(pwsc.getPortalCustomAuthenticationProvider());
+        return authenticationManagerBuilder.build();
+    }
+
+    public Mono<AuthorizationDecision> checkAccess(Mono authentication, Object object) {
+        Authentication auth = (Authentication) authentication.block();
+
+        AuthorizationContext context = (AuthorizationContext) object;
+
+        try {
+
+            PortalSecurityPrinciple prin = (PortalSecurityPrinciple) auth.getPrincipal();
+            return Mono.just(new AuthorizationDecision(decisionManager.checkAccessForPath(
+                    context.getExchange().getRequest(),
+                    auth,
+                    context.getExchange().getRequest().getPath().contextPath().value())));
+            //               hasRole("ADMIN").check(authentication, context)
+            //           .switchIfEmpty(hasRole("DBA")
+            //                                        .check(authentication, context))
+
+        } catch (Exception e) {
+            LOGGER.warn("IO exception ", e);
+        }
+        return Mono.just( new AuthorizationDecision(false));
     }
 
 }
