3e9f0eaf323d70c2e33a3bb76cb7e3544a68036c
Barry Adams <barry.david.adams@gmail.com>
2024-09-17 01:07:35 +0100

    Fix user table name

--- src/main/java/com/smokpromotion/SmokProm/domain/entity/S_User.java
+++ src/main/java/com/smokpromotion/SmokProm/domain/entity/S_User.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class S_User extends BaseSmokEntity implements Serializable {
 
     // Fields present in Majorana table.
-    private static final String TABLE_NAME = "users";
+    private static final String TABLE_NAME = "user";
 
     @Column(name="username")
     @org.springframework.data.cassandra.core.mapping.Column("username")
